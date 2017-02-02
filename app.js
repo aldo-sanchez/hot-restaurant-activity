d271967d9380c7a5732d8c6a5a1fb2d414d0a915
pruder <alexthebaker777@yahoo.com>
2017-02-01 20:05:24 -0600

    Styling still needed, reservations page in progress

---
+++ app.js
@@ -0,0 +1,12 @@
+var makebutton = "Make reservation.";
+var viewbutton = "View tables.";
+// var homebutton = ;
+var currentreservations = [];
+
+function viewtables() {
+	viewbutton.addEventListener("click", function(){});
+}
+
+function makereservation() {
+	makebutton.addEventListener("click", function(){});
+}
